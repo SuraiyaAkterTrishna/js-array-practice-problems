6fe203ae9a0946d1604540992a12b6e45b092ddf
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-12 21:57:01 +0600

    slice()

---
+++ slice.js
@@ -0,0 +1,6 @@
+const friends = [12, 25, 37, 99, 88, 47, 66, 73, 84, 51];
+const partial = friends.slice(4, 8);
+console.log(partial);
+console.log(friends);
+
+//slice() won't change the original array
