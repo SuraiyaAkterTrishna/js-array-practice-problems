cbae98fbf6141bf8ee3cc76896beac792e76e457
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-12 19:24:28 +0600

    array should check by the method of Array.isArray(friends)

---
+++ array.js
@@ -0,0 +1,12 @@
+var name = 'Bangladesh';
+var age = 50; 
+var isIndependent = true;
+var student = {id:121, class: 11, name: 'agun'};
+var friends = [11, 13, 14, 15, 16];
+
+console.log(typeof name); 
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(typeof friends); //array should check by the method of Array.isArray(friends) 
+console.log(Array.isArray(friends));
