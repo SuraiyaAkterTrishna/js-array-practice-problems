bb52c3b763f2defe15706ffd20ee8f4db95f468b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-12 19:34:26 +0600

    find elements by indexOf

--- indexOf.js
+++ indexOf.js
@@ -1,3 +1,11 @@
 // find elements by indexOf
 var friends = [11, 13, 14, 15, 16];
-console.log(friends.indexOf(15));
+console.log(friends.indexOf(15)); //found 3
+console.log(friends.indexOf(25)); //found -1, means not found
+
+if(friends.indexOf(13) !== -1){
+    console.log('Element exists in the friends array');
+}
+else {
+    console.log('Element doesnot exist');
+}
